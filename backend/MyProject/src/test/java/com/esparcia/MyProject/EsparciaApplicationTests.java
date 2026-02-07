98cc6b87e873ada2abd860e3bf26559b0d6c98fa
earlgeraldesparcia <earlgeraldesparcia@gmail.com>
2026-02-07 16:37:26 +0800

    initializing springboot

---
+++ backend/MyProject/src/test/java/com/esparcia/MyProject/EsparciaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.esparcia.MyProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsparciaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
